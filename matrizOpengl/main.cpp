46570d9d732e6c55d1fceb5c934c2fbed51a254f
rijumas <mauricio3parker@gmail.com>
2024-03-19 23:34:14 -0500

    arbolMejorado

--- matrizOpengl/main.cpp
+++ matrizOpengl/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 struct nodo {
     string id;
-    //vector<nodo> lista;
+    vector<nodo*> lista;
     nodo() {
         id = "";
     }
@@ -24,7 +24,7 @@ struct nodo {
 vector<int> generarNumerosAleatoriosUnicos(int minimo, int maximo, int cantidad) {
 
     if (cantidad > maximo - minimo + 1) {
-        cerr << "Error: Cantidad de números a generar excede el rango posible.\n";
+        cout << "Error: Cantidad de numeros a generar excede el rango posible.\n";
         return {};
     }
 
@@ -43,7 +43,7 @@ vector<int> generarNumerosAleatoriosUnicos(int minimo, int maximo, int cantidad)
     return numeros;
 }
 
-void borrar(vector<vector<nodo>> &mt) {
+void borrar(vector<vector<nodo*>> &mt) {
     vector<int> num;
     int porcentaje, profe;
     cin >> profe;
@@ -51,31 +51,50 @@ void borrar(vector<vector<nodo>> &mt) {
     num = generarNumerosAleatoriosUnicos(0, 399, porcentaje);
     
     for (int i = 0; i < num.size(); i++) {
-        cout << num[i] << " ";
-        mt[num[i] / 20][num[i] % 20].delet();
+        //cout << num[i] << " ";
+        (*(mt[num[i] / 20][num[i] % 20])).delet();
     }
 
     cout << endl;
 }
 
+/*void conectar(vector<vector<nodo*>>& mt) {
+    //laterales
+    (*(mt[10][10])).lista.push_back(mt[10][11]);
+    (*(mt[10][10])).lista.push_back(mt[10][9]);
+    //sup e infe
+    (*(mt[10][10])).lista.push_back(mt[9][10]);
+    (*(mt[10][10])).lista.push_back(mt[11][10]);
+    //diago sup
+    (*(mt[10][10])).lista.push_back(mt[9][9]);
+    (*(mt[10][10])).lista.push_back(mt[9][11]);
+    //diago inf
+    (*(mt[10][10])).lista.push_back(mt[11][9]);
+    (*(mt[10][10])).lista.push_back(mt[11][11]);
+    for (int i;; i++) {
+
+    }
+}*/
+
 int main(){
-    vector<vector<nodo>> mt(20);
+    vector<vector<nodo*>> mt(20, vector<nodo*>(20));
+    int j = 0;
     for (int i = 0;i<20; i++) {
-        for (int k = 0+(20*i);k<(20*(i+1)); k++) {
-            nodo* x = new nodo(to_string(k));
-            mt[i].push_back(*x);
+        for (int k = 0;k<20; k++) {
+            mt[i][k] = new nodo(to_string(j));
+            j++;
         }
     }
     for (int i = 0; i < 20; i++) {
         for (int k = 0; k < 20; k++) {
-            cout << mt[i][k].id << " ";
+            cout << (*(mt[i][k])).id << " ";
         }
         cout << endl;
     }
     borrar(mt);
     for (int i = 0; i < 20; i++) {
         for (int k = 0; k < 20; k++) {
-            cout << mt[i][k].id << " ";
+            cout << (*(mt[i][k])).id << " ";
         }
         cout << endl;
     }
